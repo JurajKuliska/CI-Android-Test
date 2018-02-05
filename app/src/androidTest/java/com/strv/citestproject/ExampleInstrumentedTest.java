e0a64b13cd2df61bf589e26103a0523812869814
JurajKuliska <juraj.kuliska@strv.com>
2018-02-05 18:27:52 +0100

    Initial commit

---
+++ app/src/androidTest/java/com/strv/citestproject/ExampleInstrumentedTest.java
@@ -0,0 +1,36 @@
+package com.strv.citestproject;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+
+
+	@Rule
+	public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+
+	@Test
+	public void openDetailScreen() throws Exception {
+		onView(withId(R.id.fab)).check(matches(isDisplayed())).perform(click());
+		onView(withText(R.string.detail_screen)).check(matches(isDisplayed()));
+	}
+}
