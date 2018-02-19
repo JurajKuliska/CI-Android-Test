16468306be61b137642d4d97ef3a91be265786f4
JurajKuliska <juraj.kuliska@strv.com>
2018-02-19 13:05:27 +0100

    Add a Unit test just to see if two of them work

--- app/src/test/java/com/strv/citestproject/ExampleUnitTest.java
+++ app/src/test/java/com/strv/citestproject/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package com.strv.citestproject;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 
 /**
@@ -14,5 +14,10 @@ public class ExampleUnitTest {
 	@Test
 	public void addition_isCorrect() throws Exception {
 		assertEquals(4, 2 + 2);
+	}
+
+	@Test
+	public void addition_isCorrect2() throws Exception {
+		assertEquals(6, 4 + 2);
 	}
 }
